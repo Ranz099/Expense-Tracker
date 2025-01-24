f1ccab7dd2f4a433fe6a7e51c0fb9b09d6e0a719
Ranjan Kumar <aranjan8172@gmail.com>
2025-01-24 12:02:53 +0530

    Update JournalEntryController.java

--- src/main/java/com/ranjan/myApp/controller/JournalEntryController.java
+++ src/main/java/com/ranjan/myApp/controller/JournalEntryController.java
@@ -6,6 +6,7 @@ import com.ranjan.myApp.UserRepository.UserRepository;
 import com.ranjan.myApp.model.Expense;
 import com.ranjan.myApp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,9 +46,43 @@ public class JournalEntryController {
         return journalEntryRepository.findByUser(user);
     }
 
+    @PutMapping("/{username}")
+    public String updateExpense(@PathVariable String username, @RequestBody Expense updatedExpense) {
+        // Retrieve the user from the database
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return "Username does not exist.";
+        }
+
+        // Retrieve the existing expense entry for modification
+        Expense existingExpense = journalEntryRepository.findByUserAndId(user, updatedExpense.getId());
+        if (existingExpense == null) {
+            return "Expense record not found.";
+        }
+
+        // Update the existing expense with new details
+        existingExpense.setAmount(updatedExpense.getAmount());
+        existingExpense.setTitle(updatedExpense.getTitle());
+        existingExpense.setCategory(updatedExpense.getCategory());
+        journalEntryRepository.save(existingExpense);  // Save the updated expense
+
+        return "Expense updated successfully!";  // Return a success message
+    }
+    @DeleteMapping("/{username}/{id}")
+    public String deleteExpense(@PathVariable String username, @PathVariable int id ){
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return "Username does not exist.";
+        }
+        Expense existingExpense = journalEntryRepository.findByUserAndId(user, id);
+        if (existingExpense == null) {
+            return "Expense record not found.";
+        }
+        journalEntryRepository.deleteById(id);
+        return "deleted successfully";
+    }
 
 }
-
 
 
 
