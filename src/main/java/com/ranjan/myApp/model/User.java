8af0335647bd6570814b48a49e69951f1c86fb3b
Ranjan Kumar <aranjan8172@gmail.com>
2025-01-24 12:04:24 +0530

    Update User.java

--- src/main/java/com/ranjan/myApp/model/User.java
+++ src/main/java/com/ranjan/myApp/model/User.java
@@ -13,20 +13,11 @@ public class User {
     private String password;
 
     // Getter and Setter for username
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername() {return username;}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public void setUsername(String username) {this.username = username;}
 
-    // Getter and Setter for password
-    public String getPassword() {
-        return password;
-    }
+    public String getPassword() {return password;}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public void setPassword(String password) {this.password = password;}
 }
