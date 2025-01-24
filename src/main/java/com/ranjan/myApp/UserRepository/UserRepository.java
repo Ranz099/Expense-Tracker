1acb1967450f358a8084ab43cc39abc66dcd21b4
Ranjan Kumar <aranjan8172@gmail.com>
2025-01-24 12:06:35 +0530

    Update UserRepository.java

--- src/main/java/com/ranjan/myApp/UserRepository/UserRepository.java
+++ src/main/java/com/ranjan/myApp/UserRepository/UserRepository.java
@@ -4,7 +4,6 @@ package com.ranjan.myApp.UserRepository;
 import com.ranjan.myApp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
